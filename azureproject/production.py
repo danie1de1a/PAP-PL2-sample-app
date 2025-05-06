97eba64c046dc2fd9bd1774bc6c15ce0b63a9732
dela <124284276+danie1de1a@users.noreply.github.com>
2025-05-06 17:20:27 +0200

    database update

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,8 +1,9 @@
 import os
 
-# DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-#     dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
-#     dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
-#     dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
-#     dbname=os.getenv('AZURE_POSTGRESQL_NAME')
-# )
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}'.format(
+    dbuser=os.environ['AZURE_POSTGRESQL_USER'],
+    dbpass=os.environ['AZURE_POSTGRESQL_PASSWORD'],
+    dbhost=os.environ['AZURE_POSTGRESQL_HOST'],
+    dbport=os.environ.get('AZURE_POSTGRESQL_PORT', 5432),  # Usa 5432 como valor predeterminado
+    dbname=os.environ['AZURE_POSTGRESQL_DATABASE']
+)
